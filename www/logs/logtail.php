575157d9c382f58f65683cd4d230faee72685116
Jonathan Birge <birge@mit.edu>
2023-12-19 05:05:34 +0000

    paging log interface

--- www/logs/logtail.php
+++ www/logs/logtail.php
@@ -1,45 +1,72 @@
+<?php header('Cache-Control: no-cache, no-store, must-revalidate'); ?>
+
 <table border='1'>
 <tr><th>IP Address</th><th>Host Name</th><th>Date and Time</th><th>Request</th><th>Status</th><th>Size</th></tr>
 
 <?php
 
+// Get page parameter from URL
+$page = $_GET['page'] ?? 0;
+
 // Number of lines to read from the end of the file
-$n = 16; // You can set this to any desired integer value
+$linesPerPage = 16; // You can set this to any desired integer value
 
 // Path to the CLF log file
 $logFilePath = 'access.log';
 
-// Function to get the last n lines from the log file
-function tailCustom($filePath, $lines = 1) {
-    $buffer = 4096;
-    $f = fopen($filePath, 'rb');
-    fseek($f, -1, SEEK_END);
-    if (fread($f, 1) != "\n") $lines -= 1;
-    
-    $output = '';
-    $chunk = '';
-
-    while (ftell($f) > 0 && $lines >= 0) {
-        $seek = min(ftell($f), $buffer);
-        fseek($f, -$seek, SEEK_CUR);
-        $output = ($chunk = fread($f, $seek)) . $output;
-        fseek($f, -mb_strlen($chunk, '8bit'), SEEK_CUR);
-        $lines -= substr_count($chunk, "\n");
+// Function to read the nth page from the end of the file
+function getTailPage($filePath, $linesPerPage, $page = 0) {
+    // Check if the file exists and is readable
+    if (!is_readable($filePath)) {
+        return "File not found or not readable.";
     }
 
-    while ($lines++ < 0) {
-        $output = substr($output, strpos($output, "\n") + 1);
+    // Open the file for reading
+    $fileHandle = fopen($filePath, 'r');
+    if (!$fileHandle) {
+        return "Unable to open file.";
     }
 
-    fclose($f);
-    return trim($output);
+    // Seek to the end of the file
+    fseek($fileHandle, 0, SEEK_END);
+    $fileSize = ftell($fileHandle);
+    $lines = [];
+    $currentLine = '';
+    $lineCount = 0;
+
+    // Read backwards to find the starting line
+    for ($pos = $fileSize - 2; $pos >= 0; $pos--) {
+        fseek($fileHandle, $pos);
+        $char = fgetc($fileHandle);
+
+        if ($char === "\n") {
+            // Start capturing lines after reaching the required page
+            if (++$lineCount >= $linesPerPage * ($page + 1)) {
+                break;
+            }
+            if ($lineCount > $linesPerPage * $page) {
+                array_unshift($lines, $currentLine);
+                $currentLine = '';
+            }
+        } elseif ($pos > 0) {
+            $currentLine = $char . $currentLine;
+        }
+    }
+
+    // Add the last line if the file does not end with a newline
+    if ($lineCount > $linesPerPage * $page && $currentLine !== '') {
+        array_unshift($lines, $currentLine);
+    }
+
+    fclose($fileHandle);
+    return $lines;
 }
 
 // Read the last n lines from the file
-$lastLines = explode("\n", tailCustom($logFilePath, $n));
+$lastPageLines = getTailPage($logFilePath, $linesPerPage, $page);
 
 // Process each line
-foreach ($lastLines as $line) {
+foreach ($lastPageLines as $line) {
     preg_match('/(\S+) \S+ \S+ \[(.+?)\] \"(.*?)\" (\S+) (\S+)/', $line, $matches);
 
     if (!empty($matches)) {
